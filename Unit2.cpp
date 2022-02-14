6141a2dcbbaeb5f016c0b7e00f358039fc558b5a
mishaniahomi <69090763+mishaniahomi@users.noreply.github.com>
2022-02-13 18:14:04 -0800

    Create Unit2.cpp

---
+++ Unit2.cpp
@@ -0,0 +1,31 @@
+//---------------------------------------------------------------------------
+
+#include <vcl.h>
+#pragma hdrstop
+#include <string>
+#include <fstream>
+#include <sstream>
+#include <functional>
+#include <iostream>
+#include "Unit2.h"
+#include "Unit1.h"
+
+//---------------------------------------------------------------------------
+#pragma package(smart_init)
+#pragma resource "*.dfm"
+TForm2 *Form2;
+//---------------------------------------------------------------------------
+__fastcall TForm2::TForm2(TComponent* Owner)
+	: TForm(Owner)
+{
+}
+//---------------------------------------------------------------------------
+void __fastcall TForm2::FormShow(TObject *Sender)
+{
+	//#Form2->Visible = 1;
+	//#Form2->Enabled = 1;
+}
+//---------------------------------------------------------------------------
+
+
+
